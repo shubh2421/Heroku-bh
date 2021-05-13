cc5216abfb707251aa5ad5c5dbdd997f98f2a1c6
shubh2421 <shubhankurkumar01@gmail.com>
2021-05-14 05:11:39 +0530

    step 7 end

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const session = require("express-session");
 const flash = require("express-flash");
 const { connect } = require("mongodb");
 const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
 
 // Database connection
 const url = "mongodb://localhost/pizza";
@@ -27,6 +28,7 @@ connection
   .catch((err) => {
     console.log("Connection failed...");
   });
+
 
 // // Session store
 // let mongoStore = new MongoDbStore({
@@ -47,16 +49,24 @@ app.use(
   })
 );
 
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(flash());
 
 // Assets
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Global Middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 
